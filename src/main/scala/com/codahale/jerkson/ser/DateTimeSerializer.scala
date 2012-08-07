6995244a847317c8e3c20b26a44cfd645423760d
Aaron Zinman <aaron@azinman.com>
2012-08-07 16:16:05 -0400

    Serialize to UTC time

--- src/main/scala/com/codahale/jerkson/ser/DateTimeSerializer.scala
+++ src/main/scala/com/codahale/jerkson/ser/DateTimeSerializer.scala
@@ -1,11 +1,17 @@
 package com.codahale.jerkson.ser
 
-import org.joda.time.DateTime
+import org.joda.time.{DateTime, DateTimeZone}
+import org.joda.time.format.ISODateTimeFormat
 import com.fasterxml.jackson.core.JsonGenerator
 import com.fasterxml.jackson.databind.{SerializerProvider, JsonSerializer}
 
 class DateTimeSerializer extends JsonSerializer[DateTime] {
   def serialize(value: DateTime, json: JsonGenerator, provider: SerializerProvider) {
-    json.writeString(value.toString())
+    json.writeString(DateTimeSerializer.asString(value))
   }
+}
+
+object DateTimeSerializer {
+  val fmt = ISODateTimeFormat.dateTime.withZone(DateTimeZone.UTC)
+  def asString(dateTime:DateTime) = fmt.print(dateTime)
 }
